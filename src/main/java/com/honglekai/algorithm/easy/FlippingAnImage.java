5aafcc2aa5e90b14dadbb037b7e799f3df346a3d
洪成成 <1020973308@qq.com>
2019-07-14 16:36:25 +0800

    add algorithm

---
+++ src/main/java/com/honglekai/algorithm/easy/FlippingAnImage.java
@@ -0,0 +1,40 @@
+package com.honglekai.algorithm.easy;
+
+/**
+ * description
+ * Given a binary matrix A, we want to flip the image horizontally, then invert it, and return the resulting image.
+ *
+ * To flip an image horizontally means that each row of the image is reversed.  For example, flipping [1, 1, 0] horizontally results in [0, 1, 1].
+ *
+ * To invert an image means that each 0 is replaced by 1, and each 1 is replaced by 0. For example, inverting [0, 1, 1] results in [1, 0, 0].
+ * company YH
+ *
+ * @Author hcc
+ * modifyBy
+ * createTime 2019/7/14 15:36
+ * modifyTime
+ */
+public class FlapTheImage {
+
+    public int[][] flipAndInvertImage(int[][] A) {
+        for (int[] row: A) {
+            final int LEN = row.length;
+            final int LENM1 = LEN - 1; // for calculating index.
+            for (int i = 0, bound = LEN / 2; i < bound; i++) {
+                // 1,0 -> 0,1 -> 1,0
+                // 1,1 -> 1,1 -> 0,0
+                // 0,1 -> 1,0 -> 0,1
+                // 0,0 -> 0,0 -> 1,1
+                if (row[i] == row[LENM1 - i]) {
+                    row[i] ^= 1;
+                    row[LENM1 - i] ^= 1;
+                }
+                // else do nothing.
+            }
+
+            // if odd then invert the mid number.
+            if (LEN % 2 == 1) row[LEN / 2] ^= 1;
+        }
+        return A;
+    }
+}
---
+++ src/main/java/com/honglekai/algorithm/easy/ToLowerCase709.java
@@ -0,0 +1,13 @@
+package com.honglekai.algorithm.easy;
+
+/**
+ * description
+ * company YH
+ *
+ * @Author hcc
+ * modifyBy
+ * createTime 2019/7/14 15:24
+ * modifyTime
+ */
+public class ToLowerCase709 {
+}
